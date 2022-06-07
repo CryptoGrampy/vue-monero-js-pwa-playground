eac4655985ce149f45cc8bd834f16bf8c765b793
CryptoGrampy <cryptogrampy@protonmail.com>
2022-06-07 08:20:30 +0300

    functioning

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { SimplePay, Network } from './SimplePay';
+
+export const simplePay: SimplePay = new SimplePay({
+    primaryAddress: "49ouNFXbQxj72FYjEgRjVTa35dHVrSL118vNFhxDvQWHJYpZp523EckbrqiSjM6Vb1H6Ap43qYpNRHBaVS9oBFtZUeTaH88",
+    secretViewKey: "9fb781ad709a41bd651f92c2e380813b9ca8abfb7e733105202e1d9f12799c03",
+    network: Network.mainnet,
+    defaultConfirmations: 0,
+    monerodUsername: "",
+    monerodPassword: "",
+    monerodUri: "https://community.organic-meatballs.duckdns.org:443",
+});
+
 
 createApp(App).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,71 @@
+const CopyPlugin = require("copy-webpack-plugin");
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+const { ProvidePlugin } = require("webpack");
+
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        //vue: "vue/dist/vue.runtime.esm.js",
+        fs: "html5-fs",
+      },
+      extensions: ["*", ".js", ".vue", ".json", ".ts"],
+      fallback: {
+        // browser polyfills
+        assert: require.resolve("assert"),
+        //buffer: require.resolve('buffer'),
+        //console: require.resolve('console-browserify'),
+        //constants: require.resolve('constants-browserify'),
+        crypto: require.resolve("crypto-browserify"),
+        //domain: require.resolve('domain-browser'),
+        //events: require.resolve('events'),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        path: require.resolve("path-browserify"),
+        //punycode: require.resolve('punycode'),
+        //process: require.resolve('process/browser'),
+        querystring: require.resolve("querystring-es3"),
+        stream: require.resolve("stream-browserify"),
+        //string_decoder: require.resolve('string_decoder'),
+        //sys: require.resolve('util'),
+        //timers: require.resolve('timers-browserify'),
+        //tty: require.resolve('tty-browserify'),
+        url: require.resolve("url"),
+        util: require.resolve("util"),
+        //vm: require.resolve('vm-browserify'),
+        zlib: require.resolve("browserify-zlib"),
+      },
+    },
+    externals: ["worker_threads", "ws", "perf_hooks", "child_process"], // exclude nodejs
+    plugins: [
+      new ProvidePlugin({
+        process: "process/browser",
+        Buffer: ["buffer", "Buffer"],
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "**.{js,wasm}",
+            context: path.resolve(
+              __dirname,
+              "node_modules/monero-javascript/dist"
+            ),
+            to: path.resolve(__dirname, "dist/js")
+          },
+          {
+            from: "**.{js,wasm}",
+            context: path.resolve(
+              __dirname,
+              "node_modules/monero-javascript/dist"
+            ),
+            to: path.resolve(__dirname, "dist/")
+          },
+        ],
+      }),
+    ]
+  }
 })
